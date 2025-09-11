b26aa1d018b19dc62d159e666a9116d9c522bcf3
wikitopian <wikitopian@pm.me>
2025-09-11 02:59:10 +0000

    improve testing

--- test/test.js
+++ test/test.js
@@ -1,21 +1,40 @@
 import assert from "node:assert";
+import test from "node:test";
 import SqlRite from "../SqlRite.js";
 
-const sql = new SqlRite();
+test("SqlRite.js", (t) => {
+	t.test("constructor()", async () => {
+		const sql = new SqlRite();
 
-sql.addEmployee.run({ name: "John", position: "CEO", salary: 99999 });
-sql.addEmployee.run({ name: "Jane", position: "COO", salary: 49998 });
-sql.addEmployee.run({ name: "Jack", position: "CFO", salary: 49997 });
-sql.addEmployee.run({ name: "Jill", position: "CIO", salary: 49996 });
+		sql.addEmployee.run({ name: "John", position: "CEO", salary: 99999 });
+		sql.addEmployee.run({ name: "Jane", position: "COO", salary: 49998 });
+		sql.addEmployee.run({ name: "Jack", position: "CFO", salary: 49997 });
+		sql.addEmployee.run({ name: "Jill", position: "CIO", salary: 49996 });
 
-const employee = sql.getHighestPaidEmployee.get();
+		const employee = sql.getHighestPaidEmployee.get();
+		assert(employee?.name === "John", "Highest paid employee should be John");
 
-assert(employee?.name === "John", "The highest paid employee should be John");
+		const positions = await sql.async.getPositions.all();
+		assert(positions.length === 4, "There should be four positions");
 
-sql.async.getPositions.all().then((positions) => console.log(positions));
+		sql.deleteTable();
 
-console.log(`The highest paid employee is ${employee.name}.`);
+		sql.close();
+	});
 
-sql.deleteTable();
+	t.test("getFiles()", () => {
+		const sql = new SqlRite();
 
-sql.close();
+		const files = sql.getFiles("sql");
+
+		assert(files.length > 0, "At least one SQL file in the sql directory?");
+	});
+
+	t.test("close()", () => {
+		const sql = new SqlRite();
+
+		sql.close();
+
+		assert(true, "Database closed without error");
+	});
+});
